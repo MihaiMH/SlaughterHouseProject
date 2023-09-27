eb81030cb77ebd03aeb18b946ecc7010b480ae32
Mihai <mihailamihai1603@gmail.com>
2023-09-27 21:29:39 +0200

    Initial commit

---
+++ src/main/java/com/slaughterhouse/slaughterhouseproject/SlaughterHouseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.slaughterhouse.slaughterhouseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaughterHouseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaughterHouseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slaughterhouse/slaughterhouseproject/SlaughterHouseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slaughterhouse.slaughterhouseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaughterHouseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
